fde694541c5415fa66404c7dc518b674fbfee220
Gerifield <gergo254@gmail.com>
2014-07-12 23:30:56 +0200

    Added documentation comments

--- ccphp/ccphp.php
+++ ccphp/ccphp.php
@@ -14,23 +14,70 @@ class Ccphp{
 	private $http_auth_user;
 	private $http_auth_pass;
 
+	/**
+	 * Constructor.
+	 *
+	 * Sets the server address for the queries
+	 * 
+	 * @param $url Server URL with http prefix and /api/events postix. For example: http://<server_url_here>/api/events
+	 * 
+	 */
 	public function __construct($url){
 		$this->server = $url;
 	}
 
+	/**
+	 * Default value changer.
+	 *
+	 * This function sets the default request values for all the other requests after this.
+	 * 
+	 * @param array $params Parameter array.
+	 *     Possible keys could be:
+	 *         * description 	- Description/message
+	 *         * criticality 	- Error/event level
+	 *         * category 		- Error/event category
+	 *         
+	 */
 	public function set_defaults($params = array()){
 		$this->default_params = array_merge($this->default_params, $params);
 	}
 
+	/**
+	 * HTTP authtentication configuration.
+	 * 
+	 * @param string $user HTTP auth user
+	 * @param string $pass HTTP auth pass
+	 * 
+	 */
 	public function set_auth($user = "", $pass = ""){
 		$this->http_auth_user = $user;
 		$this->http_auth_pass = $pass;
 	}
 
+	/**
+	 * Send a simple message.
+	 *
+	 * This function uses the ``send`` command and only sets the ``description`` value in the request array
+	 * 
+	 * @param  [type] $msg Message/description to send
+	 * 
+	 */
 	public function send_msg($msg){
 		$this->send(array("description" => $msg));
 	}
 
+
+	/**
+	 * Send a message to the changelog server.
+	 *
+	 * 
+	 * @param  array  $params Parameter array
+	 *     Possible keys could be:
+	 *         * description 	- Description/message
+	 *         * criticality 	- Error/event level
+	 *         * category 		- Error/event category
+	 * 
+	 */
 	public function send($params = array()){
 
 		$data = array_merge(
