c15238cc8ee73ea9bd13c4152f1cd133982c49e8
Gerifield <gergo254@gmail.com>
2014-07-12 22:58:12 +0200

    Added example php

---
+++ example.php
@@ -0,0 +1,22 @@
+<?php
+
+include "ccphp/ccphp.php";
+
+$client = new Ccphp("http://80.240.138.19/api/events");
+
+//Set all params
+$client->send(array("description" => "hello", "criticality" => 5, "category" => "info"));
+
+
+//Set message only
+$client->send(array("description" => "hello only"));
+
+
+//Set the default values or a single value
+$client->set_defaults(array("criticality" => 3));
+
+//Send a single message but with criticality 3
+$client->send(array("description" => "hello crit 3"));
+
+
+?>
